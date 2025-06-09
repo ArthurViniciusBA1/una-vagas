82dfd803ba3879291bacf8a95d843a218faff031
Arthur <arthurnaocopia@gmail.com>
2025-06-09 01:16:55 -0300

    chore: remove campo de nivel da tabela de habilidades

---
+++ prisma/migrations/20250608182556_remover_nivel_de_habilidade/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `nivel` on the `habilidades` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "habilidades" DROP COLUMN "nivel";
